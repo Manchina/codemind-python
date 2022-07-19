8a6193faaa28b7b1acee9f18cbad710ce525c875
Manchina <104674915+Manchina@users.noreply.github.com>
2022-07-19 14:28:49 +0530

    n=int(input()) for i in range(n):     k=int(input())     arr=list(map(int,input().strip().split()))     if(sorted(arr)==arr):         print("0")     else:         arr.sort()         print(abs(max(arr)-min(arr)))

---
+++ Optimal_Sorting.py
@@ -0,0 +1,10 @@
+n=int(input())
+for i in range(n):
+    k=int(input())
+    arr=list(map(int,input().strip().split()))
+    if(sorted(arr)==arr):
+        print("0")
+    else:
+        arr.sort()
+        print(abs(max(arr)-min(arr)))
+    
